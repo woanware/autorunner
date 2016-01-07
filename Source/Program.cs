994a58cb465e3919d6351f2e8eb0afc82013c433
Mark Woan <markwoan@gmail.com>
2016-01-07 09:22:58 +0000

    Updated Registry library due to case handling bug
    The Registry library (github.com/EricZimmerman/Registry) had a case
    handling bug which meant paths were not correctly identified.

--- Source/Program.cs
+++ Source/Program.cs
@@ -14,21 +14,21 @@ namespace autorunner
         [STAThread]
         static void Main()
         {
-            List<DriveMapping> dms = new List<DriveMapping>();
-            DriveMapping dm = new DriveMapping();
-            dm.MappedDrive = @"c:\";
-            dm.OriginalDrive = @"c:\";
-            dm.IsWindowsDrive = true;
-            dms.Add(dm);
+          //  List<DriveMapping> dms = new List<DriveMapping>();
+          //  DriveMapping dm = new DriveMapping();
+          //  dm.MappedDrive = @"c:\";
+          //  dm.OriginalDrive = @"c:\";
+          //  dm.IsWindowsDrive = true;
+          //  dms.Add(dm);
 
-          AutoRunEntry autoRunEntry = new AutoRunEntry();
-          autoRunEntry = Helper.GetFilePathWithNoParameters(dms, autoRunEntry, @"C:\windows\system32\cmd.exe /D /C start C:\windows\system32\ie4uinit.exe -ClearIconCache");
+          //AutoRunEntry autoRunEntry = new AutoRunEntry();
+          //autoRunEntry = Helper.GetFilePathWithNoParameters(dms, autoRunEntry, @"C:\windows\system32\cmd.exe /D /C start C:\windows\system32\ie4uinit.exe -ClearIconCache");
 
              //AutoRunEntry autoRunEntry = new AutoRunEntry();
              //autoRunEntry = Helper.GetFilePathWithNoParameters(dms, autoRunEntry, @"c:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\IDE\PrivateAssemblies\Microsoft.VisualStudio.QualityTools.RecorderBarBHO100.dll");
              
 
-            Console.WriteLine(autoRunEntry.FilePath);
+            //Console.WriteLine(autoRunEntry.FilePath);
 
             //return;
 
